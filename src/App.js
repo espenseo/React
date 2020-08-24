c68601ba23da2ec78c9a83af5c300fc6fa18e408
espen <crom2031@gmail.com>
2020-08-25 00:31:17 +0900

    2-1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'; # 리액트를 불러와서 사용할 수 있게 해줌
+import logo from './logo.svg'; #SVG파일을 import하는 코드
+import './App.css'; #CSS파일을 import하는코드 
+
+function App() { ##함수형 컴포넌트
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+##이런 코드를 JSX라고 함
+export default App;
