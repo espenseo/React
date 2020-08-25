4174a3fccb868c63bbf5990760c07b37ed0e3bc3
espen <crom2031@gmail.com>
2020-08-25 20:50:14 +0900

    2-2

---
+++ JSX.js
@@ -0,0 +1,13 @@
+function App() {
+    return(
+        <div>
+            Hello <b>react</b>
+        </div>
+    );
+}
+
+
+
+## funtction App() {
+##    return React.createElement("div", null, "Hello", React.createElement("b", null, "react"));
+##}
