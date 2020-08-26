f573100fe8678c579658c4e1f98fcc02df4ca698
espen <crom2031@gmail.com>
2020-08-26 20:49:54 +0900

    5.1.2-1

---
+++ CH5/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ValidationSample from './src/ValidationSample';
+ 
+class App extends Component {
+  render() {
+    return (
+      <ValidationSample/>
+    );
+  }
+}
+ 
+export default App;
