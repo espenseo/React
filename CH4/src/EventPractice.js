4c7e5c7f702d6e9e9efa1b5f12c310d21d9b6884
espen <crom2031@gmail.com>
2020-08-26 20:18:04 +0900

    4.2.3.2-1

--- CH4/src/EventPractice.js
+++ CH4/src/EventPractice.js
@@ -1,49 +1,32 @@
-import React, { Component } from ‘react‘;
-
-
+import React, { Component } from 'react';
+ 
 class EventPractice extends Component {
-
-
-
-state = {
-    message: “
+ 
+  state = {
+    message: ''
   }
-
-
-
-constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-
-
-handleChange(e) {
+ 
+  handleChange = (e) => {
     this.setState({
       message: e.target.value
     });
   }
-
-
-
-handleClick() {
+ 
+  handleClick = () => {
     alert(this.state.message);
     this.setState({
-      message: “
+      message: ''
     });
   }
-
-
-
-render() {
+ 
+  render() {
     return (
       <div>
         <h1>이벤트 연습</h1>
         <input
-          type=“text“
-          name=“message“
-          placeholder=“아무거나 입력해 보세요“
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
         />
@@ -52,7 +35,5 @@ render() {
     );
   }
 }
-
-
-
+ 
 export default EventPractice;
