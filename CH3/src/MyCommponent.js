1fd6e130f98152d9c52e01f9f42a6f540c98f70d
espen <crom2031@gmail.com>
2020-08-26 19:43:05 +0900

    3.3.6.2

--- CH3/src/MyCommponent.js
+++ CH3/src/MyCommponent.js
@@ -3,4 +3,21 @@ import PropTypes from ‘prop-types‘;
 
 
 const MyComponent = ({ name, children }) => {
-(…)
+  return (…);
+};
+
+
+
+MyComponent.defaultProps = {
+  name: ‘기본 이름‘
+};
+
+
+
+MyComponent.propTypes = {
+  name: PropTypes.string
+};
+
+
+
+export default MyComponent;
