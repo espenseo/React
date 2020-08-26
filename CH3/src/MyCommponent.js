6d5e428685082593bb287e8ba9f22cd135b5f121
espen <crom2031@gmail.com>
2020-08-26 19:47:43 +0900

    3.3.6.1-1

--- CH3/src/MyCommponent.js
+++ CH3/src/MyCommponent.js
@@ -1,11 +1,32 @@
 import React from ‘react‘;
-import MyComponent from ‘./MyComponent‘;
+import PropTypes from ‘prop-types‘;
 
 
-const App = () => {
-  return <MyComponent name={3}>리액트</MyComponent>;
+const MyComponent = ({ name, favoriteNumber, children }) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {name}입니다. <br />
+      children 값은 {children}
+      입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
+    </div>
+  );
 };
 
 
 
-export default App;
+MyComponent.defaultProps = {
+  name: ‘기본 이름‘
+};
+
+
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired
+};
+
+
+
+export default MyComponent;
