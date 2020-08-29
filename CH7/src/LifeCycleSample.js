ed38f4a98d851bc6c2bfa6d3e14ab01a82c7994e
espen <crom2031@gmail.com>
2020-08-29 10:07:18 +0900

    7.3.3-1

--- CH7/src/LifeCycleSample.js
+++ CH7/src/LifeCycleSample.js
@@ -77,8 +77,31 @@ componentDidUpdate(prevProps, prevState, snapshot) {
 
 
 
-render() {
-    console.log(‘render‘);
+  render() {
+    console.log(‘render’);
+
+
+<span class="co46">const</span> <span class="co32">style</span> <span class="co35">=</span><span class="co33"> {</span>
+
+
+      color: this.props.color
+    };
+
+
+
+<span class="co46">return</span><span class="co33"> (</span>
+
+
+      <div>
+        {this.props.missing.value}
+        <h1 style={style} ref={ref => (this.myRef = ref)}>
+          {this.state.number}
+        </h1>
+        <p>color: {this.state.color}</p>
+        <button onClick={this.handleClick}>더하기</button>
+      </div>
+    );
+  }
 
 
 
