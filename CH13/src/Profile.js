4325e1bc649342a784eea160720d98b4ea51abeb
espen <crom2031@gmail.com>
2020-08-30 08:49:08 +0900

    13.6.4-1

--- CH13/src/Profile.js
+++ CH13/src/Profile.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { withRouter } from ‘react-router-dom‘;
-import WithRouterSample from ‘./WithRouterSample‘;
- 
-const Profile = ({ match }) => {
-    const { username } = match.params;
-    const profile = data[username];
-    if (!profile) {
-      return <div>존재하지 않는 사용자입니다.</div>;
-    }
-    return (
+import React from ‘react‘;
+import { NavLink, Route } from ‘react-router-dom‘;
+import Profile from ‘./Profile‘;
+
+
+const Profiles = () => {
+  const activeStyle = {
+    background: ‘black‘,
+    color: ‘white‘
+  };
+  return (
     <div>
       <h3>사용자 목록:</h3>
       <ul>
         <li>
-          <Link to="/profiles/velopert">velopert</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/velopert" active>
+            velopert
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/gildong">gildong</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/gildong">
+            gildong
+          </NavLink>
         </li>
       </ul>
  
