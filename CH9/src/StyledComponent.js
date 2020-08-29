b23ac6e46662420a9722821b222090e8eb91ee4b
espen <crom2031@gmail.com>
2020-08-29 11:52:20 +0900

    9.4.3-1

--- CH9/src/StyledComponent.js
+++ CH9/src/StyledComponent.js
@@ -3,11 +3,11 @@ import styled, { css } from ‘styled-components‘;
 
 
 const Box = styled.div</span>
-  <span class="co44">/* props</span><span class="co44">로</span> <span class="co44">넣어</span> <span class="co44">준</span> <span class="co44">값을</span> <span class="co44">직접</span> <span class="co44">전달해</span> <span class="co44">줄</span> <span class="co44">수</span> <span class="co44">있습니다</span><span class="co44">. */</span>
-  <span class="co33">background</span><span class="co34">:</span> <span class="co49">${</span><span class="co33">props</span> <span class="co46">=&gt;</span> <span class="co34">props</span><span class="co33">.</span><span class="co34">color</span> <span class="co35">||</span> <span class="co31">'</span><span class="co31">blue</span><span class="co31">'</span><span class="co49">}</span><span class="co36">;</span>
-  <span class="co33">padding</span><span class="co34">:</span> <span class="co32">1rem</span><span class="co36">;</span>
-  <span class="co33">display</span><span class="co34">:</span> <span class="co33">flex</span><span class="co36">;</span>
-<span class="co31">;
+  /* props로 넣어 준 값을 직접 전달해 줄 수 있습니다. */
+  background: ${props => props.color || 'blue'};
+  padding : 1rem;
+  display: flex;
+  ;
 
 
 
