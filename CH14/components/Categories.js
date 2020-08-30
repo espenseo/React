d9bf3062409ef7ab52e1e224ab3673df6ad27fbb
espen <crom2031@gmail.com>
2020-08-30 09:35:02 +0900

    14.7.3-1

--- CH14/components/Categories.js
+++ CH14/components/Categories.js
@@ -1,48 +1,60 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
- 
+import React from ‘react‘;
+import styled from ‘styled-components‘;
+import { NavLink } from ‘react-router-dom‘;
+
+
 const categories = [
-  (...)
+  (…)
 ];
- 
-const CategoriesBlock = styled.div`
-  (...)
-`;
- 
-const Category = styled.div`
+
+
+
+const CategoriesBlock = styled.div</span>
+  <span class="co33">(...)</span>
+<span class="co31">;
+
+
+
+const Category = styled(NavLink)`
   font-size: 1.125rem;
   cursor: pointer;
   white-space: pre;
   text-decoration: none;
   color: inherit;
   padding-bottom: 0.25rem;
- 
-  &:hover {
+
+
+
+&:hover {
     color: #495057;
   }
- 
-  ${props =>
-    props.active && css`
-      font-weight: 600;
-      border-bottom: 2px solid #22b8cf;
-      color: #22b8cf;
-      &:hover {
-        color: #3bc9db;
-      }
-  `}
- 
-  & + & {
+
+
+
+&.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+    &:hover {
+      color: #3bc9db;
+    }
+  }
+
+
+
+& + & {
     margin-left: 1rem;
   }
-`;
-const Categories = ({ onSelect, category }) => {
-  return (
-    <CategoriesBlock>
-      {categories.map(c => (
-        <Category
-          key={c.name}
-          active={category === c.name}
-          onClick={() => onSelect(c.name)}
+</span><span class="co33">;</span>
+<span class="co46">const</span> <span class="co47">Categories</span> <span class="co35">=</span> <span class="co33">()</span> <span class="co46">=&gt;</span> <span class="co33">{</span>
+  <span class="co46">return</span> <span class="co33">(</span>
+    <span class="co33">&lt;</span><span class="co48">CategoriesBlock</span><span class="co33">&gt;</span>
+      <span class="co49">{</span><span class="co34">categories</span><span class="co33">.</span><span class="co47">map</span><span class="co33">(c </span><span class="co46">=&gt;</span><span class="co33"> (</span>
+<span class="co33">        &lt;</span><span class="co48">Category</span>
+          <span class="co32">key</span><span class="co33">={</span><span class="co34">c</span><span class="co33">.</span><span class="co34">name</span><span class="co33">}</span>
+          <span class="cd2 co32">activeClassName</span><span class="cd2 co33">=</span><span class="cd2 co31">"</span><span class="cd2 co31">active</span><span class="cd2 co31">"</span>
+          <span class="cd2 co32">exact</span><span class="cd2 co33">={</span><span class="cd2 co34">c</span><span class="cd2 co33">.</span><span class="cd2 co34">name</span><span class="cd2 co33"> === </span><span class="cd2 co31">'</span><span class="cd2 co31">all</span><span class="cd2 co31">'</span><span class="cd2 co33">}</span>
+          <span class="cd2 co32">to</span><span class="cd2 co33">={</span><span class="cd2 co34">c</span><span class="cd2 co33">.</span><span class="cd2 co34">name</span><span class="cd2 co33"> === </span><span class="cd2 co31">'</span><span class="cd2 co31">all</span><span class="cd2 co31">'</span><span class="cd2 co33"> ? </span><span class="cd2 co31">'</span><span class="cd2 co31">/</span><span class="cd2 co31">'</span><span class="cd2 co33"> : </span><span class="cd2 co31">/${c.name}`}
         >
           {c.text}
         </Category>
@@ -50,5 +62,7 @@ const Categories = ({ onSelect, category }) => {
     </CategoriesBlock>
   );
 };
- 
+
+
+
 export default Categories;
