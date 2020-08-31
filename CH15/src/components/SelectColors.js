8eeeae81efa1d5640711d6b42ec4dc37bd0eb4ff
espen <crom2031@gmail.com>
2020-08-31 10:01:22 +0900

    15.4.2-2

--- CH15/src/components/SelectColors.js
+++ CH15/src/components/SelectColors.js
@@ -1,26 +1,37 @@
-import React, { Component } from ‘react‘;
-import ColorContext from ‘../contexts/color‘;
-
-
-const colors = [‘red‘, ‘orange‘, ‘yellow‘, ‘green‘, ‘blue‘, ‘indigo‘, ‘violet‘];
-
-
-
+import React, { Component } from 'react';
+import ColorContext from '../contexts/color';
+ 
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+ 
 class SelectColors extends Component {
   static contextType = ColorContext;
+ 
+  handleSetColor = color => {
+    this.context.actions.setColor(color);
+  };
+ 
+  handleSetSubcolor = subcolor => {
+    this.context.actions.setSubcolor(subcolor);
+  };
+ 
   render() {
     return (
       <div>
         <h2>색상을 선택하세요.</h2>
-        <div style={{ display: ‘flex‘ }}>
+        <div style={{ display: 'flex' }}>
           {colors.map(color => (
             <div
               key={color}
               style={{
                 background: color,
-                width: ‘24px‘,
-                height: ‘24px‘,
-                cursor: ‘pointer‘
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer'
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={e => {
+                e.preventDefault();
+                this.handleSetSubcolor(color);
               }}
             />
           ))}
@@ -30,7 +41,5 @@ class SelectColors extends Component {
     );
   }
 }
-
-
-
+ 
 export default SelectColors;
