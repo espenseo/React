487421aa92f4d461d4bf49724b5322cea761b9d7
espen <crom2031@gmail.com>
2020-09-01 08:07:24 +0900

    17.6.1.2-3

--- CH17/modules/todos.js
+++ CH17/modules/todos.js
@@ -36,25 +36,23 @@ export const remove = createAction(REMOVE, id => id);
   
   const todos = handleActions(
     {
-      [CHANGE_INPUT]: (state, action) => ({ …state, input: action.payload }),
-      [INSERT]: (state, action) => ({
-        …state,
-        todos: state.todos.concat(action.payload),
+      [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+      [INSERT]: (state, { payload: todo }) => ({
+        ...state,
+        todos: state.todos.concat(todo),
       }),
-      [TOGGLE]: (state, action) => ({
-        …state,
+      [TOGGLE]: (state, { payload: id }) => ({
+        ...state,
         todos: state.todos.map(todo =>
-          todo.id = = = action.payload ? { …todo, done: !todo.done } : todo,
+          todo.id = = = id ? { ...todo, done: !todo.done } : todo,
         ),
       }),
-      [REMOVE]: (state, action) => ({
-        …state,
-        todos: state.todos.filter(todo => todo.id != = action.id),
+      [REMOVE]: (state, { payload: id }) => ({
+        ...state,
+        todos: state.todos.filter(todo => todo.id != = id),
       }),
     },
     initialState,
   );
-  
-  
   
   export default todos;
