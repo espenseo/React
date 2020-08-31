c1e8bbd47d2f50fb9ce15acefd7e793e46797cdd
espen <crom2031@gmail.com>
2020-08-31 10:10:39 +0900

    16.2.5-1

--- CH16/src/index.js
+++ CH16/src/index.js
@@ -1,6 +1,6 @@
 const divToggle = document.querySelector(‘.toggle‘);
 const counter = document.querySelector(‘h1‘);
-const btnIncrease = document.querySelector(‘#id‘);
+const btnIncrease = document.querySelector(‘#increase‘);
 const btnDecrease = document.querySelector(‘#decrease‘);
 
 
@@ -13,3 +13,10 @@ const DECREASE = ‘DECREASE‘;
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = difference => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
+
+
+
+const initialState = {
+  toggle: false,
+  counter: 0
+};
