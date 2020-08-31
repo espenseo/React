f0690965d92fbf2109179056f0cf7a8d72219d5c
espen <crom2031@gmail.com>
2020-09-01 08:28:52 +0900

    18.3.1.4-1

---
+++ CH18/lib/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+
+export const getPost = id =>
+  axios.get(https://jsonplaceholder.typicode.com/posts/</span><span class="co49">${</span><span class="cd2 co33">id</span><span class="co33">}</span><span class="cd2 co31">);
+
+
+
+export const getUsers = id =>
+  axios.get(https://jsonplaceholder.typicode.com/users);
