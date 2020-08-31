0563227504da72522ff6ad1180adad05c3b8d1a5
espen <crom2031@gmail.com>
2020-09-01 07:38:23 +0900

    16.2.10-1

--- CH16/src/index.js
+++ CH16/src/index.js
@@ -5,9 +5,9 @@ const render = () => {
   const state = store.getState(); // 현재 상태를 불러옵니다.
   // 토글 처리
   if (state.toggle) {
-    divToggle.classList.add(‘active‘);
+    divToggle.classList.add('active');
   } else {
-    divToggle.classList.remove(‘active‘);
+    divToggle.classList.remove('active');
   }
   // 카운터 처리
   counter.innerText = state.counter;
