5369417d5219db5a7b8c8ce38013c7b62325b50d
espen <crom2031@gmail.com>
2020-09-01 07:49:39 +0900

    17.4.1-1

---
+++ CH17/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
+
+
+const store = createStore(rootReducer);
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+serviceWorker.unregister();
