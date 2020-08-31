4b49b9acd949b9c40bceba8f8b72fae3ef4391fd
espen <crom2031@gmail.com>
2020-09-01 08:04:15 +0900

    17.6.1.1-2

--- CH17/modules/counter.js
+++ CH17/modules/counter.js
@@ -1,4 +1,4 @@
-import { createAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -7,26 +7,15 @@ export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
 const initialState = {
-    number: 0
-  };
-  
-  
-  
-  function counter(state = initialState, action) {
-    switch (action.type) {
-      case INCREASE:
-        return {
-          number: state.number + 1
-        };
-      case DECREASE:
-        return {
-          number: state.number - 1
-        };
-      default:
-        return state;
-    }
-  }
-  
-  
-  
-  export default counter;
+  number: 0,
+};
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+
+export default counter;
