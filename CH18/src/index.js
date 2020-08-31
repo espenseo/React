8697668eed520b35b7c596853a1a62b0c4906244
espen <crom2031@gmail.com>
2020-09-01 08:56:53 +0900

    18.3.2.2-3

--- CH18/src/index.js
+++ CH18/src/index.js
@@ -9,11 +9,16 @@ import rootReducer from './modules';
 // import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
-
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger, ReduxThunk, sagaMiddleware)
+);
+sagaMiddleware.run(rootSaga);
 
 
 
