2c535b3347f2e7630526f380aa5a83bd434cd6cf
espen <crom2031@gmail.com>
2020-09-02 09:35:27 +0900

    19.2.2-1

--- CH19/src/App.js
+++ CH19/src/App.js
@@ -3,30 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 
-class App extends Component {
-    state = {
-      SplitMe: null
-    };
-    handleClick = async () => {
-      const loadedModule = await import('./SplitMe');
-      this.setState({
-        SplitMe: loadedModule.default
-      });
-    };
-    render() {
-      const { SplitMe } = this.state;
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p onClick={this.handleClick}>Hello React!</p>
-            {SplitMe && <SplitMe />}
-          </header>
-        </div>
-      );
-    }
-  }
-  
-  
-  
-  export default App;
+const SplitMe = React.lazy(() => import(‘./SplitMe‘));
+
+
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading…</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
