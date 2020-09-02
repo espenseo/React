fc796e6e0c1d999a3ebb324e25f5a476cd6071c1
espen <crom2031@gmail.com>
2020-09-02 09:36:54 +0900

    19.2.3-1

--- CH19/src/App.js
+++ CH19/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-
-const SplitMe = React.lazy(() => import(‘./SplitMe‘));
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'));
 
 
 function App() {
@@ -16,9 +15,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading…</div>}>
           {visible && <SplitMe />}
-        </Suspense>
       </header>
     </div>
   );
