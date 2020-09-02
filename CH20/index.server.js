db6ab663ef369f330474c51055d75560531d1c0b
espen <crom2031@gmail.com>
2020-09-02 10:26:31 +0900

    20.4.4-2

--- CH20/index.server.js
+++ CH20/index.server.js
@@ -9,6 +9,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './modules';
+import PreloadContext from './lib/PreloadContext';
+
 
 // asset-manifest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
@@ -55,24 +57,42 @@ const app = express();
 
 
 // 서버 사이드 렌더링을 처리할 핸들러 함수입니다.
-const serverRender = (req, res, next) => {
+const serverRender = async (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줍니다.
 
 
 
 const context = {};
   const store = createStore(rootReducer, applyMiddleware(thunk));
+
+
+
+const preloadContext = {
+    done: false,
+    promises: []
+  };
   const jsx = (
-    <Provider store={store}>
-      <StaticRouter location={req.url} context={context}>
-        <App />
-      </StaticRouter>
-    </Provider>
+    <PreloadContext.Provider value={preloadContext}>
+      <Provider store={store}>
+        <StaticRouter location={req.url} context={context}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    </PreloadContext.Provider>
   );
-  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
+
+
+
+ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한번 렌더링합니다.
+  try {
+    await Promise.all(preloadContext.promises); // 모든 프로미스를 기다립니다.
+  } catch (e) {
+    return res.status(500);
+  }
+  preloadContext.done = true;
+  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 합니다.
   res.send(createPage(root)); // 결과물을 응답합니다.
 };
-
 
 
 const serve = express.static(path.resolve('./build'), {
