512c51a8a0a29b456b3b2411c7f3ecb900eab656
espen <crom2031@gmail.com>
2020-09-03 09:40:40 +0900

    22.7.1-3

--- CH22/src/api/posts/posts.ctrl.js
+++ CH22/src/api/posts/posts.ctrl.js
@@ -1,7 +1,28 @@
 import Post from '../../models/post';
 
 
-export const write = ctx => {};
+/*
+  POST /api/posts
+  {
+    title: '제목',
+    body: '내용',
+    tags: ['태그1', '태그2']
+  }
+*/
+export const write = async ctx => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 
 
