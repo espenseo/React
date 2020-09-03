5bdc105c78242d43eb9075f48751fee86bc78a72
espen <crom2031@gmail.com>
2020-09-03 09:19:09 +0900

    21.5.5.2-3

--- CH21/blog/blog-backend/src/api/posts/index.js
+++ CH21/blog/blog-backend/src/api/posts/index.js
@@ -1,14 +1,7 @@
 const Router = require('koa-router');
+const postsCtrl = require('./posts.ctrl');
+
 const posts = new Router();
-
-
-const printInfo = ctx => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params,
-  };
-};
 
 
 
