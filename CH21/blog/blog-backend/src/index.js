ecf73e3b11aefdedeed174ba0bbf8fe24d714706
espen <crom2031@gmail.com>
2020-09-03 09:00:04 +0900

    21.3.2-3

--- CH21/blog/blog-backend/src/index.js
+++ CH21/blog/blog-backend/src/index.js
@@ -6,7 +6,7 @@ const app = new Koa();
 app.use((ctx, next) => {
     console.log(ctx.url);
     console.log(1);
-    if (ctx.query.authorized != = '1') {
+    if (ctx.query.authorized !== '1') {
         ctx.status = 401; // Unauthorized
         return;
       }
