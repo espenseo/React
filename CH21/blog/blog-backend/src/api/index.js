f4c69655765ca64c3a6efa212650e5f6315b6185
espen <crom2031@gmail.com>
2020-09-03 09:14:23 +0900

    21.5.5-2

--- CH21/blog/blog-backend/src/api/index.js
+++ CH21/blog/blog-backend/src/api/index.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router');
+cconst posts = require('./posts');
+
+
 const api = new Router();
 
 
-api.get('/test', ctx => {
-  ctx.body = 'test 성공';
-});
+
+api.use('/posts', posts.routes());
 
 
 
