7bf7a69d05f64b460035828a7a906e49810431d2
espen <crom2031@gmail.com>
2020-09-04 10:27:42 +0900

    23.5.5-1

--- CH23/src/api/auth/auth.ctrl.js
+++ CH23/src/api/auth/auth.ctrl.js
@@ -115,3 +115,23 @@ export const logout = async ctx => {
       ctx.throw(500, e);
     }
   };
+
+  export const getPostById = async (ctx, next) => {
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+      ctx.status = 400; // Bad Request
+      return;
+    }
+    try {
+      const post = await Post.findById(id);
+      // 포스트가 존재하지 않을 때
+      if (!post) {
+        ctx.status = 404; // Not Found
+        return;
+      }
+      ctx.state.post = post;
+      return next();
+    } catch (e) {
+      ctx.throw(500, e);
+    }
+  };
