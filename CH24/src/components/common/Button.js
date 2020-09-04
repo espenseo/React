382c60e5122fc9a3d940c04058b61022ffdd7375
espen <crom2031@gmail.com>
2020-09-04 10:56:31 +0900

    24.2.1.2-2

--- CH24/src/components/common/Button.js
+++ CH24/src/components/common/Button.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import palette from '../../lib/styles/palette';
-
 
 const StyledButton = styled.button`
   border: none;
@@ -12,19 +11,28 @@ const StyledButton = styled.button`
   color: white;
   outline: none;
   cursor: pointer;
-
-
-
-background: ${palette.gray[8]};
+  background: ${palette.gray[8]};
   &:hover {
     background: ${palette.gray[6]};
   }
+  ${props =>
+    props.fullWidth &&
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
+  ${props =>
+    props.cyan &&
+    css`
+      background: ${palette.cyan[5]};
+      &:hover {
+        background: ${palette.cyan[4]};
+      }
+    `}
 `;
 
-
-
-const Button = props => <StyledButton {…props} />;
-
-
+const Button = props => <StyledButton {...props} />;
 
 export default Button;
