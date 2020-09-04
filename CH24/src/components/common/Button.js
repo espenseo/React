fbd7381f5595a32416fc5e480be507961a7975d8
espen <crom2031@gmail.com>
2020-09-04 12:00:30 +0900

    24.3.1-4

--- CH24/src/components/common/Button.js
+++ CH24/src/components/common/Button.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { withRouter } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
+
 
 const StyledButton = styled.button`
   border: none;
@@ -11,28 +13,53 @@ const StyledButton = styled.button`
   color: white;
   outline: none;
   cursor: pointer;
-  background: ${palette.gray[8]};
+
+
+
+background: ${palette.gray[8]};
   &:hover {
     background: ${palette.gray[6]};
   }
-  ${props =>
+
+
+
+${props =>
     props.fullWidth &&
     css`
-      padding-top: 0.75rem;
-      padding-bottom: 0.75rem;
-      width: 100%;
-      font-size: 1.125rem;
+        padding-top: 0.75rem;
+        padding-bottom: 0.75rem;
+        width:100%;
+        font-size: 1.125rem;
     `}
-  ${props =>
+
+
+
+${props =>
     props.cyan &&
     css`
-      background: ${palette.cyan[5]};
-      &:hover {
-        background: ${palette.cyan[4]};
-      }
+        background: ${palette.cyan[5]};
+        &:hober{
+            background: ${palette.cyan[4]};
+        }
     `}
 `;
 
-const Button = props => <StyledButton {...props} />;
 
-export default Button;
+
+const Button = ({ to, history, …rest }) => {
+  const onClick = e => {
+    // to가 있다면 to로 페이지 이동
+    if (to) {
+      history.push(to);
+    }
+    if (rest.onClick) {
+      rest.onClick(e);
+    }
+  };
+  return <StyledButton {…rest} onClick={onClick} />;
+};
+
+
+
+export default withRouter(Button);
+
