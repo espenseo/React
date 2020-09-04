133d4054472b130e2376a4e96b24a0242f28ec8d
espen <crom2031@gmail.com>
2020-09-04 12:18:19 +0900

    24.3.3-2

--- CH24/src/modules/user.js
+++ CH24/src/modules/user.js
@@ -11,12 +11,12 @@ const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
   'user/CHECK',
 );
-
+const LOGOUT = 'user/LOGOUT';
 
 
 export const tempSetUser = createAction(TEMP_SET_USER, user => user);
 export const check = createAction(CHECK);
-
+export const logout = createAction(LOGOUT);
 
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
@@ -31,12 +31,22 @@ function checkFailureSaga() {
   }
   
   
+  function* logoutSaga() {
+    try {
+      yield call(authAPI.logout); // logout API 호출
+      localStorage.removeItem('user'); // localStorage에서 user를 제거
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  
   
   export function* userSaga() {
     yield takeLatest(CHECK, checkSaga);
     yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+    yield takeLatest(LOGOUT, logoutSaga);
   }
-
 
 const initialState = {
   user: null,
@@ -60,6 +70,10 @@ export default handleActions(
       …state,
       user: null,
       checkError: error,
+    }),
+    [LOGOUT]: state => ({
+        …state,
+        user: null,
     }),
   },
   initialState,
