6a6eb76732ddcdd4a26b2e497813184612663a9e
espen <crom2031@gmail.com>
2020-09-04 09:20:13 +0900

    23.3.2-1

--- CH23/src/api/auth/auth.ctrl.js
+++ CH23/src/api/auth/auth.ctrl.js
@@ -28,22 +28,40 @@ export const register = async ctx => {
 
 
 
-const { username, password } = ctx.request.body;
-  try {
-    // username이 이미 존재하는지 확인
-    const exists = await User.findByUsername(username);
-    if (exists) {
-      ctx.status = 409; // Conflict
+  /*
+  POST /api/auth/login
+  {
+    username: 'velopert',
+    password: 'mypass123'
+  }
+*/
+export const login = async ctx => {
+  const { username, password } = ctx.request.body;
+
+
+// username, password가 없으면 에러 처리
+  if (!username || !password) {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+
+
+
+try {
+    const user = await User.findByUsername(username);
+    // 계정이 존재하지 않으면 에러 처리
+    if (!user) {
+      ctx.status = 401;
       return;
     }
-
-const user = new User({
-    username,
-});
-await user.setPassword(password); // 비밀번호 설정
-await user.save(); // 데이터베이스에 저장
-  ctx.body = user.serialize();
-} catch (e) {
+    const valid = await user.checkPassword(password);
+    // 잘못된 비밀번호
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  } catch (e) {
     ctx.throw(500, e);
   }
 };
