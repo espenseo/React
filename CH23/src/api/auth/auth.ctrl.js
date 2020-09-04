6e43838c63ad3fd32b289b7e6b8def9dd39014b2
espen <crom2031@gmail.com>
2020-09-04 10:29:00 +0900

    23.5.5-4

--- CH23/src/api/auth/auth.ctrl.js
+++ CH23/src/api/auth/auth.ctrl.js
@@ -139,3 +139,12 @@ export const logout = async ctx => {
   export const read = ctx => {
     ctx.body = ctx.state.post;
   };
+
+  export const checkOwnPost = (ctx, next) => {
+    const { user, post } = ctx.state;
+    if (post.user._id.toString() !== user._id) {
+      ctx.status = 403;
+      return;
+    }
+    return next();
+  };
