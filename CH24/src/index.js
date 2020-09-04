e5cb94740569320bccf7969e5817143ec0fc4854
espen <crom2031@gmail.com>
2020-09-04 12:14:14 +0900

    24.3.2.2-3

--- CH24/src/index.js
+++ CH24/src/index.js
@@ -9,6 +9,8 @@ import { createStore, applyMiddleware  } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
+import { tempSetUser, check } from './modules/user';
+
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -16,7 +18,26 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
+
+
+function loadUser() {
+  try {
+    const user = localStorage.getItem('user');
+    if (!user) return; // 로그인 상태가 아니라면 아무것도 안 함
+
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+} catch (e) {
+    console.log('localStorage is not working');
+  }
+}
+
+
+
 sagaMiddleware.run(rootSaga);
+loadUser();
+
+
 
 ReactDOM.render(
   <Provider store={store}>
@@ -24,7 +45,9 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById(‘root‘),
 );
+
+
 
 serviceWorker.unregister();
