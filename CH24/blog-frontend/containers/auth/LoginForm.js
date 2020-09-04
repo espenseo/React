dbaee73f76529fe7c2f1c73af2c49951bd505f9e
espen <crom2031@gmail.com>
2020-09-04 12:09:54 +0900

    24.3.2.2-1

--- CH24/blog-frontend/containers/auth/LoginForm.js
+++ CH24/blog-frontend/containers/auth/LoginForm.js
@@ -55,6 +55,11 @@ const onSubmit = e => {
   useEffect(() => {
     if (user) {
       history.push('/');
+      try {
+        localStorage.setItem('user', JSON.stringify(user));
+      } catch (e) {
+        console.log('localStorage is not working');
+      }
     }
   }, [history, user]);
 
