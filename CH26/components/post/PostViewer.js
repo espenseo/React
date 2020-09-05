ff073aea994efb4664e916c21d9a5ed2537cf665
espen <crom2031@gmail.com>
2020-09-05 16:32:20 +0900

    26.2.1-6

--- CH26/components/post/PostViewer.js
+++ CH26/components/post/PostViewer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
+import SubInfo from '../common/SubInfo';
+import Tags from '../common/Tags';
 
 
 const PostViewerBlock = styled(Responsive)`
@@ -58,36 +60,26 @@ const PostViewer = ({ post, error, loading }) => {
       return <PostViewerBlock>오류 발생!</PostViewerBlock>;
     }
   
-  
-  // 로딩 중이거나 아직 포스트 데이터가 없을 때
+    // 로딩 중이거나 아직 포스트 데이터가 없을 때
     if (loading || !post) {
       return null;
     }
   
-  
-  
-  const { title, body, user, publishedDate, tags } = post;
+    const { title, body, user, publishedDate, tags } = post;
     return (
       <PostViewerBlock>
         <PostHead>
           <h1>{title}</h1>
-          <SubInfo>
-            <span>
-              <b>{user.username}</b>
-            </span>
-            <span>{new Date(publishedDate).toLocaleDateString()}</span>
-          </SubInfo>
-          <Tags>
-            {tags.map(tag => (
-              <div className="tag">#{tag}</div>
-            ))}
-          </Tags>
+          <SubInfo
+            username={user.username}
+            publishedDate={publishedDate}
+            hasMarginTop
+          />
+          <Tags tags={tags} />
         </PostHead>
         <PostContent dangerouslySetInnerHTML={{__html: body }} />
       </PostViewerBlock>
     );
   };
-  
-  
   
   export default PostViewer;
