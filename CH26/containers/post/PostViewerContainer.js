dc114535032a7699e698595929e803aa5398d512
espen <crom2031@gmail.com>
2020-09-05 16:36:33 +0900

    26.2.2-4

--- CH26/containers/post/PostViewerContainer.js
+++ CH26/containers/post/PostViewerContainer.js
@@ -1,35 +1,40 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import qs from 'qs';
 import { withRouter } from 'react-router-dom';
-import { readPost, unloadPost } from '../../modules/post';
-import PostViewer from '../../components/post/PostViewer';
+import { useDispatch, useSelector } from 'react-redux';
+import PostList from '../../components/posts/PostList';
+import { listPosts } from '../../modules/posts';
 
 
-const PostViewerContainer = ({ match }) => {
-  // 처음 마운트될 때 포스트 읽기 API 요청
-  const { postId } = match.params;
+const PostListContainer = ({ location }) => {
   const dispatch = useDispatch();
-  const { post, error, loading } = useSelector(({ post, loading }) => ({
-    post: post.post,
-    error: post.error,
-    loading: loading['post/READ_POST'],
-  }));
+  const { posts, error, loading, user } = useSelector(
+    ({ posts, loading, user }) => ({
+      posts: posts.posts,
+      error: posts.error,
+      loading: loading['posts/LIST_POSTS'],
+      user: user.user,
+    }),
+  );
+  useEffect(() => {
+    const { tag, username, page } = qs.parse(location.search, {
+      ignoreQueryPrefix: true,
+    });
+    dispatch(listPosts({ tag, username, page }));
+  }, [dispatch, location.search]);
 
 
 
-useEffect(() => {
-    dispatch(readPost(postId));
-    // 언마운트될 때 리덕스에서 포스트 데이터 없애기
-    return () => {
-      dispatch(unloadPost());
-    };
-  }, [dispatch, postId]);
-
-
-
-return <PostViewer post={post} loading={loading} error={error} />;
+return (
+    <PostList
+      loading={loading}
+      error={error}
+      posts={posts}
+      showWriteButton={user}
+    />
+  );
 };
 
 
 
-export default withRouter(PostViewerContainer);
+export default withRouter(PostListContainer);
