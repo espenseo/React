b07e4cf09482ef3b40cf2d4803ba096598fd37ce
espen <crom2031@gmail.com>
2020-09-05 15:31:23 +0900

    25.1-2

---
+++ CH25/pages/WritePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Editor from '../components/write/Editor';
+import Responsive from '../components/common/Responsive';
+
+
+const WritePage = () => {
+  return (
+    <Responsive>
+      <Editor />
+    </Responsive>
+  );
+};
+
+
+
+export default WritePage;
