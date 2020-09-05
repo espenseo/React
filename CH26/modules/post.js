f0874f9cc338f56bb8d41dbb0cff42534b170cc2
espen <crom2031@gmail.com>
2020-09-05 16:43:35 +0900

    26.2.4-2

--- CH26/modules/post.js
+++ CH26/modules/post.js
@@ -29,26 +29,28 @@ export function* postsSaga() {
 
 
 const initialState = {
-  posts: null,
-  error: null,
-};
-
-
-
-const posts = handleActions(
-  {
-    [LIST_POSTS_SUCCESS]: (state, { payload: posts }) => ({
-      …state,
-      posts,
-    }),
-    [LIST_POSTS_FAILURE]: (state, { payload: error }) => ({
-      …state,
-      error,
-    }),
-  },
-  initialState,
-);
-
-
-
-export default posts;
+    posts: null,
+    error: null,
+    lastPage: 1,
+  };
+  
+  
+  
+  const posts = handleActions(
+    {
+      [LIST_POSTS_SUCCESS]: (state, { payload: posts, meta: response }) => ({
+        …state,
+        posts,
+        lastPage: parseInt(response.headers['last-page'], 10), // 문자열을 숫자로 변환
+      }),
+      [LIST_POSTS_FAILURE]: (state, { payload: error }) => ({
+        …state,
+        error,
+      }),
+    },
+    initialState,
+  );
+  
+  
+  
+  export default posts;
