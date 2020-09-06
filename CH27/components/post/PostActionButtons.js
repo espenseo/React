a82e44a7501b3999e5aa91533f0e2c55d94393eb
espen <crom2031@gmail.com>
2020-09-06 09:01:32 +0900

    27.1.2-3

--- CH27/components/post/PostActionButtons.js
+++ CH27/components/post/PostActionButtons.js
@@ -28,15 +28,13 @@ const ActionButton = styled.button`
     }
 `;
 
-const PostActionButtons = () => {
-  return (
-    <PostActionButtonsBlock>
-      <ActionButton>수정</ActionButton>
-      <ActionButton>삭제</ActionButton>
-    </PostActionButtonsBlock>
-  );
-};
-
-
-
-export default PostActionButtons;
+const PostActionButtons = ({ onEdit }) => {
+    return (
+      <PostActionButtonsBlock>
+        <ActionButton onClick={onEdit}>수정</ActionButton>
+        <ActionButton>삭제</ActionButton>
+      </PostActionButtonsBlock>
+    );
+  };
+  
+  export default PostActionButtons;
